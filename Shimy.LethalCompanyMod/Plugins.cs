4995ffedfca08d19afd8bb5072a705f7bf007225
Shimychu <phenizer@hotmail.com>
2024-06-11 16:49:20 -0700

    Add project files.

---
+++ Shimy.LethalCompanyMod/Plugins.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Shimy.LethalCompanyMod.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shimy.LethalCompanyMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ShimyModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Shimy.LCInfiniteSprintMod";
+        private const string modName = "Shimy Sprint Mod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ShimyModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                    Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Shimy Sprint Mod.");
+
+            harmony.PatchAll(typeof(ShimyModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+
+        }
+    }
+}
+
